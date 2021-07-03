03e9822d64bd95039b4ff953940bf2a0255a6e10
Matveyx25 <matveyx25@gmail.com>
2021-07-04 00:02:01 +0400

    gh-pages

--- app/src/App.js
+++ app/src/App.js
@@ -71,7 +71,7 @@ const AppContainer = compose(
    connect(mapStateToProps))(App)
 
 const MainApp = props => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <AppContainer/>
     </Provider>
--- app/src/Components/Navigation/Navigation.jsx
+++ app/src/Components/Navigation/Navigation.jsx
@@ -8,18 +8,18 @@ const Navigation = props => {
         <div className={s.wrapper}>
             <ul>
                 <li><NavLink to={'/'} exact activeClassName={s.active}>
-                    <img src="/icons/Home.png" alt=""/>
+                    <img src="./icons/Home.png" alt=""/>
                     <span className={s.line}></span>
                     </NavLink></li>
                 <li><NavLink to={'/book'} activeClassName={s.active}>
-                    <img src="/icons/Book.png" alt=""/>
+                    <img src="./icons/Book.png" alt=""/>
                     <span className={s.line}></span>
                     </NavLink></li>
                 {/* <li><NavLink to={'/add'}>
                     <img src="/icons/Add.png" activeClassName={s.activeLink} alt=""/>
                     </NavLink></li> */}
                 <li><NavLink to={'/search'} activeClassName={s.active}>
-                    <img src="/icons/Search.png" alt=""/>
+                    <img src="./icons/Search.png" alt=""/>
                     <span className={s.line}></span>
                     </NavLink></li>
                 {/* <li><NavLink to={'/profile'}>
