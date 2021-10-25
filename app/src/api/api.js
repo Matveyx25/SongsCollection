432478d2993d9d048242c76f927f3bc59d1fdc6f
Matveyx25 <matveyx25@gmail.com>
2021-10-25 21:19:25 +0400

    New api port

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 
 const instance = axios.create({
-    baseURL : 'http://213.139.208.216/' 
+    baseURL : 'http://213.139.208.216:5000/' 
 })
 
 // export const usersAPI = {
